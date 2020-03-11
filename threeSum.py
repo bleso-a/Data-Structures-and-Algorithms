0f68ce94d9c12661c39a6beef42bd4d1a7c164c6
Blessing Adetoye Adesiji <38926059+bleso-a@users.noreply.github.com>
2020-03-11 16:17:20 +0100

    Add files via upload
    Implementation of Three sum algorithm challenge. 
    Time Complexity - O(n^2)
    Space Complexity - O(n)

---
+++ threeSum.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 16:07:39 2020
+
+@author: Blessing
+"""
+
+def threeNumberSum(array, targetSum):
+    # O(n^2) time | O(n) Space
+	array.sort()
+	triplets = []
+	for i in range(len(array) - 2):
+		left = i + 1
+		right = len(array) - 1
+		while left < right:
+			currentSum = array[i] + array[left] + array[right]
+			if currentSum == targetSum:
+				triplets.append([array[i], array[left], array[right]])
+				left += 1
+				right -= 1
+			elif currentSum < targetSum:
+				left += 1
+			elif currentSum > targetSum:
+				right -= 1
+	return triplets
